4c531fe9b2def133b69722453863f82618a10420
Настя Карпова <soul.houston000@gmail.com>
2023-06-07 22:16:42 +0300

    I made the title more look like in the task.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,34 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
 
 
 function App() {
   return (
-<nav class="navbar navbar-expand-xl navbar-light bg-light">
-  <a class="navbar-brand" href="#">Search for books</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarText">
-    <ul class="navbar-nav mr-auto">
+<nav class="navbar navbar-light">
+ 
+  <h1 className="greeting">Search for books</h1>
+
+  
+
+  <form action="#" id="navbar-search" className="navbar_search" >
+      <input class="input navbar_input" />
+      <span> 
+
+      </span>
+    </form>
+
+ 
+  <div class="" id="navbarText">
+     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Cathegories <span class="sr-only"></span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Sorting by</a>
+        <a class="nav-link">Cathegories <span class="sr-only"></span></a>
       </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
+     <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" >Sorting by</a>
+      </li>
+    </ul>
   </div>
 
 </nav>
