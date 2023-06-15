0cade67657e9e88c172958f79e53c98d4bbfefbe
Настя Карпова <soul.houston000@gmail.com>
2023-06-15 22:45:14 +0300

    fix search-button and book images

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -12,28 +12,25 @@ function App() {
 
 function handleChange(event) {
  const book = event.target.value;
-setBook(book);
+ setBook(book);
 }
 function handleSubmit(event) {
   event.preventDefault();
-  axios.get("https://www.googleapis.com/books/v1/volumes?q="+book+"&key="+apiKey+"&maxResult=30")
+  axios.get("https://www.googleapis.com/books/v1/volumes?q="+book+"&key="+apiKey+"&maxResult=40")
  .then(data =>{
   console.log(data.data.items);
   setResult(data.data.items);
  })
 }
-
-
   return (
+    <div>
 <nav class="navbar navbar-light">
- 
-  <h1 className="greeting mt-10">Search for books</h1>
-
+  <h1 className="greeting">Search for books</h1>
   <form action="#" id="navbar-search" onSubmit={handleSubmit} className="navbar_search mt-10" >
       <input class="input navbar_input" type="text" onChange={handleChange} placeholder="Type to search..." autoComplete='on' data-path="0.0.0.0.2.0.1" />
       <span> 
       <div class="input-group-text" data-path="0.0.0.0.2.0.0">
-                    <button class="btn btn-sm text-secondary" type="button" data-path="0.0.0.0.2.0.0.0">
+                    <button onClick={handleSubmit} class="btn btn-sm text-secondary"  type="button" data-path="0.0.0.0.2.0.0.0">
                       <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg" data-config-id="auto-svg-2-5" data-path="0.0.0.0.2.0.0.0.0">
                         <path d="M20.7 19.3L17 15.6C20.1 11.7 19.5 6 15.6 2.9C11.7 -0.2 5.99999 0.5 2.89999 4.3C-0.200006 8.2 0.499995 13.9 4.29999 17C7.59999 19.6 12.3 19.6 15.6 17L19.3 20.7C19.7 21.1 20.3 21.1 20.7 20.7C21.1 20.3 21.1 19.7 20.7 19.3ZM9.99999 17C6.09999 17 2.99999 13.9 2.99999 10C2.99999 6.1 6.09999 3 9.99999 3C13.9 3 17 6.1 17 10C17 13.9 13.9 17 9.99999 17Z" fill="currentColor" data-path="0.0.0.0.2.0.0.0.0.0"></path>
                       </svg>
@@ -42,7 +39,7 @@ function handleSubmit(event) {
       </span>
     </form>
 
-
+    <div>
   <div class="" id="navbarText" className="navbar_cath">
      <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
@@ -83,16 +80,21 @@ function handleSubmit(event) {
     <a class="dropdown-item" href="#">newest</a>
   </div>
 </div>
-{result.map(book => (
-  <a target='_blank' href={book.volumeInfo.previewLink}>
-    <img src={book.volumeInfo.imageLinks !== undefined ? book.volumeInfo.imageLinks.thumbnail : ''} alt={book.title} />
-  </a>
-))
-
-}
+  </div>
   </div>
 
+<div>
+
+</div>
+
 </nav>
+
+ {result.map(book => (
+  <a target='_blank' href={book.volumeInfo.previewLink}>
+    <img src={book.volumeInfo.imageLinks !== undefined ? book.volumeInfo.imageLinks.thumbnail : ''} alt={book.title} />
+  </a>))}
+
+</div>
   );
 }
 export default App;
