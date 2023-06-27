f30c85b052ec93293733a6228c6774b037f921b8
Настя Карпова <soul.houston000@gmail.com>
2023-06-27 18:48:23 +0300

    final homepage

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,18 +4,20 @@ import './index.css';
 import App from './App';
 import Main from './Main.js';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 <React.StrictMode>
+  <HashRouter basename={windows.location.pathname || ''}>
 <Router>
 <Routes>
 <Route path='/' element={<App />} />
 <Route path='/more' element={<Main />} />
 </Routes>
 </Router>
+</HashRouter>
 </React.StrictMode>
   </React.StrictMode>
 );
